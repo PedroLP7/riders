4e341a82dcca47cad881cf233c24209639581b1b
PedroLP7 <113777475+PedroLP7@users.noreply.github.com>
2024-02-26 17:53:32 +0100

    Update Customer.php

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -5,9 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Customer extends Model
+class customer extends Model
 {
+    use HasFactory;
     protected $table = 'customer';
     protected $primaryKey = 'id_customer';
     public $timestamps = false;
+
+    public function deliverys(){
+        return $this->hasMany(delivery::class, 'id_customer_fk');
+    }
 }
