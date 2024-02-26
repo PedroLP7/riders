d6d12b6cb611ea424b52ca7eb4dda8c121fe28b6
isaacRuizCalero <113252055+isaacRuizCalero@users.noreply.github.com>
2024-02-26 18:50:10 +0100

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,7 @@ Route::middleware(['auth'])->group(function () {
     route::get('/admin', [UsuarioController::class,'admin']);
 });
 Route::resource('usuario', App\Http\Controllers\UsuarioController::class);
+
+route::get('/map', function(){
+    return view('testMapbox');
+});
