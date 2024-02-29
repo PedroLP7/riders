6b141a6e2bb9fd8b9ded649d10972fbe6d4eace1
AriveroM <ariverom2223@politecnics.barcelona>
2024-02-29 19:42:01 +0100

    AHORAAAA

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,5 +3,4 @@ import * as bootstrap from 'bootstrap';
 import { createApp } from 'vue'
 import landing from '../Components/landing.vue'
 
-createApp(landing).mount('landing');
-
+createApp(landing).mount("#landing")
