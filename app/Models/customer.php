76894056b21265475320cb70aa42cca872835b1d
JVPpolitecnics <dnerizb2223@politecnics.barcelona>
2024-03-04 17:20:11 +0100

    api pua insertada

--- app/Models/customer.php
+++ app/Models/customer.php
@@ -10,6 +10,7 @@ class customer extends Model
     use HasFactory;
      protected $table = 'customer';
    protected $primaryKey = 'id_customer';
+   public $timestamps = false;
    public function deliverys(){
     return $this->hasMany(delivery::class, 'id_customer_fk');
 }
