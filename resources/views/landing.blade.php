7f1bf69b40598cfb5112545bf9705cc8b0cc2e71
AriveroM <ariverom2223@politecnics.barcelona>
2024-03-04 16:22:06 +0100

    cipot

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">  
   <link rel="stylesheet" type="text/css" href="../resources/css/styles.css">  
-  @vite(['resources/css/app.css','resources/css/app.scss', 'resources/js/app.js']), 
+  @vite(['resources/css/app.css','resources/css/app.scss', 'resources/js/app.js'])
   <title>Registro de Riders</title>
 </head>
 <body class="landing">
