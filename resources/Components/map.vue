ca04c9a276809b071e26c793e824c03841f05c56
AriveroM <ariverom2223@politecnics.barcelona>
2024-03-07 20:02:12 +0100

    rth

--- resources/Components/map.vue
+++ resources/Components/map.vue
@@ -20,33 +20,77 @@
       </form>
     </div>
   </div>
+  <div v-if="isMarkerOptionsModalOpen" class="modal-overlay" @click="closeMarkerOptionsModal">
+    <div class="modal" @click.stop>
+      <span class="close" @click="closeMarkerOptionsModal">&times;</span>
+      <button type="button" @click="removeMarker">Remover PUA</button>
+    </div>
+  </div>
 </template>
-
 
 <script>
 import mapboxgl from 'mapbox-gl';
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
 
-
 export default {
   setup() {
     const mapContainer = ref(null);
     const isModalOpen = ref(false);
+    const isMarkerOptionsModalOpen = ref(false);
     const mendigo = ref({
       Xcoord: '',
       Ycoord: '',
       location: '',
     });
-    const mendigos = ref([]);
-
+    const selectedMarker = ref(null);
     let map;
+
+    onMounted(async () => {
+      mapboxgl.accessToken = 'pk.eyJ1IjoiaXNhYWNydWlpaXoiLCJhIjoiY2xzdW94NjlkMDd5azJrcWttem82M3RsNSJ9.5DxmiuHnmt9-z0I-eds7RQ';
+      map = new mapboxgl.Map({
+        container: mapContainer.value,
+        style: 'mapbox://styles/mapbox/dark-v10',
+        zoom: 20
+      });
+
+      const geolocateControl = new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true
+        },
+        trackUserLocation: true,
+        showUserLocation: true,
+        fitBoundsOptions: {
+          maxZoom: 20
+        }
+      });
+
+      map.addControl(geolocateControl);
+
+      geolocateControl.on('geolocate', (e) => {
+        map.easeTo({
+          pitch: 45,
+          center: [e.coords.longitude, e.coords.latitude],
+          zoom: 13
+        });
+      });
+
+      map.on('load', () => {
+        geolocateControl.trigger();
+        selectMendigos();
+      });
+
+      map.on('click', (e) => {
+        mendigo.value.Xcoord = e.lngLat.lng.toFixed(5);
+        mendigo.value.Ycoord = e.lngLat.lat.toFixed(5);
+        fetchStreetName(e.lngLat.lng, e.lngLat.lat);
+      });
+    });
 
     const selectMendigos = async () => {
       try {
-        const response = await axios.get('customer'); 
-        mendigos.value = response.data;
-        mendigos.value.forEach((m) => {
+        const response = await axios.get('customer');
+        response.data.forEach((m) => {
           addMarker(m);
         });
       } catch (error) {
@@ -58,61 +102,30 @@ export default {
       const lng = parseFloat(m.Xcoord);
       const lat = parseFloat(m.Ycoord);
       if (!isNaN(lng) && !isNaN(lat)) {
-        new mapboxgl.Marker()
+        const marker = new mapboxgl.Marker()
           .setLngLat([lng, lat])
-          .setPopup(new mapboxgl.Popup({ offset: 25 })
-          .setText(`Calle: ${m.location}`))
-          .addTo(map);        
+          .setPopup(new mapboxgl.Popup({ offset: 25 }).setText(`Calle: ${m.location}`))
+          .addTo(map);
+        
+        marker.getElement().addEventListener('click', (e) => {
+          e.stopPropagation();
+          selectedMarker.value = { marker, data: m };
+          isMarkerOptionsModalOpen.value = true;
+        });
       } else {
         console.error('Coordenadas no válidas:', m.Xcoord, m.Ycoord);
       }
     };
 
-    const insertMendigo = async () => {
-      try {       
-        await axios.post('customer', mendigo.value);           
-      } catch (error) {
-        console.error('Error al insertar mendigo:', error);
+    const removeMarker = () => {
+      if (selectedMarker.value) {
+        selectedMarker.value.marker.remove();
+        // LLamar delete
+        isMarkerOptionsModalOpen.value = false;
       }
     };
 
-    onMounted(async () => {
-      mapboxgl.accessToken = 'pk.eyJ1IjoiaXNhYWNydWlpaXoiLCJhIjoiY2xzdW94NjlkMDd5azJrcWttem82M3RsNSJ9.5DxmiuHnmt9-z0I-eds7RQ';
-      map = new mapboxgl.Map({
-        container: mapContainer.value,
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center: [2.17, 41.38],
-        zoom: 15,
-    });
-
-      const geolocateControl = new mapboxgl.GeolocateControl({
-            positionOptions: {
-              enableHighAccuracy: true
-            },
-            trackUserLocation: true,
-            showUserLocation: true,
-            fitBoundsOptions: {
-              maxZoom: 15
-            }
-      });
-
-      map.addControl(geolocateControl);
-
-      map.on('load', () => {
-    
-        geolocateControl.trigger();
-      });
-
-      map.on('click', (e) => {
-        mendigo.value.Xcoord = e.lngLat.lng.toFixed(5);
-        mendigo.value.Ycoord = e.lngLat.lat.toFixed(5);
-        fetchStreetName(e.lngLat.lng, e.lngLat.lat);
-      });
-
-      await selectMendigos();
-    });
-
-    async function fetchStreetName(longitude, latitude) {
+    const fetchStreetName = async (longitude, latitude) => {
       const geocodingUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=${mapboxgl.accessToken}`;
       try {
         const response = await fetch(geocodingUrl);
@@ -122,77 +135,49 @@ export default {
       } catch (error) {
         console.error('Error al realizar reverse geocoding:', error);
       }
-    }
+    };
 
-    function closeModal() {
+    const confirmAddMarker = () => {
+      addMarker(mendigo.value);
       isModalOpen.value = false;
-    }
+      insertMendigo();
+    };
 
-    function confirmAddMarker() {
-      addMarker(mendigo.value);      
-      closeModal();
-        insertMendigo();
-    }
+    const insertMendigo = async () => {
+      try {
+        await axios.post('customer', mendigo.value);
+        selectMendigos(); // Refrescar los marcadores
+      } catch (error) {
+        console.error('Error al insertar mendigo:', error);
+      }
+    };
 
-    return { mapContainer, isModalOpen, closeModal, confirmAddMarker, mendigo };
+    const closeModal = () => {
+      isModalOpen.value = false;
+    };
+
+    const closeMarkerOptionsModal = () => {
+      isMarkerOptionsModalOpen.value = false;
+    };
+
+    return {
+      mapContainer,
+      isModalOpen,
+      closeModal,
+      confirmAddMarker,
+      mendigo,
+      isMarkerOptionsModalOpen,
+      closeMarkerOptionsModal,
+      removeMarker
+    };
   },
 };
 </script>
-
-
-
-
-
-
-
-
-
 
 <style scoped>
 .map-container {
   height: 100vh;
   width: 100vw;
-}
-
-.modal-overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.modal {
-  background-color: white;
-  padding: 20px;
-  border-radius: 8px;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-}
-
-.close {
-  float: right;
-  font-size: 1.5rem;
-  cursor: pointer;
-}
-
-.form-group {
-  margin-bottom: 1rem;
-}
-
-.form-group label {
-  display: block;
-  margin-bottom: .5rem;
-}
-
-.form-group input[type="text"] {
-  width: 100%;
-  padding: 0.5rem;
-  border: 1px solid #ccc;
-  border-radius: 4px;
 }
 
 button {
