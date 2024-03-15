2336999ab65436b6785805cef3fbbfbc48378942
isaacruiiiz <103143448+isaacruiiiz@users.noreply.github.com>
2024-03-15 18:41:29 +0100

    asdfghjklñ

--- resources/views/auth/loginForm.blade.php
+++ resources/views/auth/loginForm.blade.php
@@ -4,43 +4,31 @@
 
 
 @include('php_partials.mensajes')
+@vite(['resources/css/loginFormStyles.css'])
+
 <form method="POST" action="{{action([App\Http\Controllers\UsuarioController::class,'authenticate'])}}" >
     @csrf
 
-    <div class="card">
-        <div class="card-title mt-3 px-3 ">
+    <h3>{{'Iniciar Sesión'}}</h3>
 
-
-        <h3>{{'Login'}}</h3>
-        </div>
-
-
-        <div class="card-body">
-            <div class="form-group row px-2">
-                <label for="username" class="col-sm-2 col-form-label">Nombre</label>
-                <div class="col-sm-10">
-                  <input type="text" class="form-control" id="username" name="user_name" value="{{old('username')}}">
-                </div>
-            </div>
-            <div class="form-group row mt-3 px-2">
-              <label for="password" class="col-sm-2 col-form-label">Password</label>
+    <div class="card"> 
+      <div class="card-body">
+          <div class="form-group row px-2">
               <div class="col-sm-10">
-                <input type="password" class="form-control" id="password" name="pswd" value="{{old('password')}}">
+                <input type="text" class="form-control" id="username" name="user_name" value="{{old('username')}}" placeholder="Nombre de Usuario">
               </div>
+          </div>
+          <div class="form-group row mt-3 px-2">
+            <div class="col-sm-10">
+              <input type="password" class="form-control" id="password" name="pswd" value="{{old('password')}}" placeholder="Contraseña">
             </div>
+          </div>
 
+          <button class="btn btn-primary"type="submit">Aceptar</button>
+      </div>
+    </div>
 
-
-
-            <button  class="btn btn-primary"type="submit">Aceptar</button>
-
-          </form>
-
-            </div>
-
-
-        </div>
-
+</form>
 
 
 
