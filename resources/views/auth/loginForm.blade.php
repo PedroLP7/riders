b232c43987c2ff7620eb2ebcb56ac29d0230c52e
isaacruiiiz <103143448+isaacruiiiz@users.noreply.github.com>
2024-03-18 11:14:16 +0100

    electrocutame pedrito

--- resources/views/auth/loginForm.blade.php
+++ resources/views/auth/loginForm.blade.php
@@ -6,6 +6,8 @@
 @include('php_partials.mensajes')
 @vite(['resources/css/loginFormStyles.css'])
 
+<script src="https://code.iconify.design/iconify-icon/2.0.0/iconify-icon.min.js"></script>
+
 <form method="POST" action="{{action([App\Http\Controllers\UsuarioController::class,'authenticate'])}}" >
     @csrf
 
@@ -21,6 +23,8 @@
           <div class="form-group row mt-3 px-2">
             <div class="col-sm-10">
               <input type="password" class="form-control" id="password" name="pswd" value="{{old('password')}}" placeholder="Contraseña">
+              <iconify-icon class="eye-active" icon="mdi:eye" height="24"></iconify-icon>
+              <iconify-icon class="eye-unactive" icon="ph:eye-closed" height="24"></iconify-icon>
             </div>
           </div>
 
@@ -29,6 +33,53 @@
     </div>
 
 </form>
+
+<script>
+
+  // placeholder show/hide
+
+  const usernameInput = document.getElementById('username');
+  const passwordInput = document.getElementById('password');
+
+  usernameInput.addEventListener('click', () => {
+    usernameInput.placeholder = '';
+  });
+
+  usernameInput.addEventListener('blur', () => {
+    usernameInput.placeholder = 'Nombre de Usuario';
+  });
+
+  passwordInput.addEventListener('click', () => {
+    passwordInput.placeholder = '';
+  });
+
+  passwordInput.addEventListener('blur', () => {
+    passwordInput.placeholder = 'Contraseña';
+  });
+
+  // toggle password visibility
+
+  let eyeActive = document.querySelector('.eye-active');
+  let eyeUnactive = document.querySelector('.eye-unactive');
+
+  eyeActive.style.display = 'none';
+
+  eyeActive.addEventListener('click', () => {
+    eyeActive.style.display = 'none';
+    eyeUnactive.style.display = 'block';
+    passwordInput.type = 'text';
+  });
+
+  eyeUnactive.addEventListener('click', () => {
+    eyeUnactive.style.display = 'none';
+    eyeActive.style.display = 'block';
+    passwordInput.type = 'password';
+  });
+
+
+
+
+</script>
 
 
 
