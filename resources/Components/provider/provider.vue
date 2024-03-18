fc110801e6d5fd5d9f1c9d80518c558f29fbaed0
isaacruiiiz <103143448+isaacruiiiz@users.noreply.github.com>
2024-03-19 00:20:53 +0100

    electrocutame pedrito

--- resources/Components/provider/provider.vue
+++ resources/Components/provider/provider.vue
@@ -6,42 +6,29 @@
 
         <button class="btn btn-primary" id="crearPack" @click="showCreateMenu()" type="button">Crear Pack</button>
         
-        <button class="btn btn-primary" @click="showBook()">Show bookings</button>
-        <div class="card mt-3" style="width: 18rem;" v-for="menu in user.provider.menus">
+        <!-- <button class="btn btn-primary" @click="showBook()">Show bookings</button> -->
+        <div id="cards-container">
+        <div class="card mt-3" v-for="menu in user.provider.menus">
 
             <div class="card-body">
-                <h5 class="card-title">Menu {{ menu.id_menu }} </h5>
                 <img src="../../images/menu.png" class="card-img-top" alt="imgmenu" id="imgmenu">
-                <p class="card-text"> item1 :{{ menu.item1 }}
-                    <br>
+                <h5 class="card-title">Pack #{{ menu.id_menu }} </h5>
+                <p class="card-text"> 
+                    item1 :{{ menu.item1 }}
+                    
                     item 2 :{{ menu.item2 }}
-                    <br>
+                    
                     item 3 :{{ menu.item3 }}
-                    <br>
+                    
                     cantidad : {{ menu.pivot.quantity }}
                     <!-- id : {{  idUser }} -->
-
                 </p>
 
 
             </div>
-            <button class="btn btn-primary">Editar menu</button>
+            <button class="btn btn-primary" id="botonEditar">Editar</button>
         </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        </div>
     </div>
     <div class="container" id="bookings">
     <bookings v-if="showBookings" :usuario="idUser"/>
@@ -130,6 +117,7 @@ export default {
 
 * {
     font-family: "Outfit", sans-serif;
+    box-sizing: content-box;
 }
 
 *::selection {
@@ -151,16 +139,111 @@ body {
 }
 
 #crearPack {
+    display: flex;
+    align-items: center;
+    justify-content: center;
     position: fixed;
-    width: 50%;
-    height: 7%;
+    width: 40%;
+    height: 6%;
     top: 81%;
     right: 5%;
     z-index: 999;
     border: none;
+    border-radius: 19px;
     background-color: #8bb481ba;
     backdrop-filter: blur(9px);
     color: #1E1E1E;
+    font-weight: bold;
+    font-size: 24px;
+}
+
+#cards-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+}
+
+.card {
+    background-color: #393939;
+    width: 90%;
+    height: 129px;
+    flex-direction: row;
+    --bs-card-spacer-y: 0;
+    --bs-card-spacer-x: 0;
+    --bs-card-title-spacer-y: 0;
+    --bs-card-border-width: 0;
+    border-radius: 26px;
+    margin-right: 7px;
+    margin-left: 7px;
+}
+
+.mt-3 {
+    margin-top: 0.8rem !important;
+}
+
+.card-body {
+    background-color: #393939;
+    border: none;
+    width: 374px;
+    height: 129px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    border-radius: 26px;
+}
+
+#imgmenu {
+    position: absolute;
+    left: 10px;
+    height: 85%;
+    width: auto;
+    border-radius: 19px;
+}
+
+.card-title {
+    position: absolute;
+    top: 14px;
+    left: 44%;
+    color: #FFFFFF;
+    font-size: 24px;
+    margin-left: 15px;
+}
+
+.card-text {
+    position: absolute;
+    left: 44%;
+    top: 43px;
+    color: #8F8F8F;
+    font-size: 15px;
+    margin-left: 15px;
+    margin-right: 30px;
+}
+
+#botonEditar {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    width: 50px;
+    height: 15px;
+    border-radius: 50px;
+    border: none;
+    bottom: 0;
+    right: 0;
+    background-color: #8BB481;
+    color: #1E1E1E;
+    font-size: 13px;
+    font-weight: bold;
+    margin-bottom: 10px;
+    margin-right: 10px;
+}
+
+#botonEditar:hover {
+    color: #1E1E1E;
+    background-color: #8BB481;
+    border-color: none;
 }
 
 </style>
