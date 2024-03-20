8ce4d619fa16e00bc6a71ecf2efd5835b5030f42
PedroLP7 <113777475+PedroLP7@users.noreply.github.com>
2024-03-20 16:07:16 +0100

    A

--- resources/Components/provider/editMenu.vue
+++ resources/Components/provider/editMenu.vue
@@ -3,10 +3,30 @@
 
         aqui se edita el menu clickado
 
+
+
+
+
+
     </div>
 </template>
 <script>
 export default {
+    data() {
+        return {
+            prueba: {},
+        };
+    },
+    methods: {
+
+        prueba2() {
+            const prueba = document.getElementById('editMenu').getAttribute('data-id');
+            console.log(prueba);
+        }
+    },
+     created() {
+        this.prueba2();
+    }
 
 }
 </script>
