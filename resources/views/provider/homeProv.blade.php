859165ed71e819123f2236f853aa1207dddb3f7c
PedroLP7 <113777475+PedroLP7@users.noreply.github.com>
2024-03-21 19:13:03 +0100

    Update homeProv.blade.php

--- resources/views/provider/homeProv.blade.php
+++ resources/views/provider/homeProv.blade.php
@@ -8,17 +8,17 @@
     @vite(['resources/css/app.css','resources/css/app.scss', 'resources/js/app.js'])
 </head>
 <body>
-    @if (Auth::check())
+    {{-- @if (Auth::check())
     {{Auth::user()->user_name}} {{Auth::user()->user_type->type_name}}
 
-    <h2>Bienvenido/a {{Auth::user()->user_name}} tu rol es de {{Auth::user()->user_type->type_name}}</h2>
 
-    @endif
+
+    @endif --}}
 
 
     <div id="provider"></div>
 
-    {{-- <a class="btn btn-primary" href="{{url('logout')}}"> LOGOUT TEMPORAL</a> --}}
+
 </body>
 </html>
 
