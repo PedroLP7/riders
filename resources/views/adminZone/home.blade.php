b0d25651c90a37f6ea39b5738cc4a9e7e6b0ee5a
PedroLP7 <113777475+PedroLP7@users.noreply.github.com>
2024-04-14 11:22:42 +0200

    a

--- resources/views/adminZone/home.blade.php
+++ resources/views/adminZone/home.blade.php
@@ -59,82 +59,71 @@
 
 
 
+{{-- </div> --}}
+<div  class="row">
+    <div class="col-md-12">
+
+    <table class="table table-dark table-hover">
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">UserName</th>
+          <th scope="col">RealName</th>
+
+          <th scope="col">UserType</th>
+
+
+          <th>Dni/Cif</th>
+
+          <th>Active</th>
+
+
+        </tr>
+
+      <tbody>
+
+          @foreach ($usuarios as $usuario)
+         <td>{{$usuario->id_user}}</td>
+         <td>{{$usuario->user_name}}</td>
+         <td>{{$usuario->real_name}}</td>
+
+       <td>{{$usuario->user_type->type_name}}</td>
+
+
+          <td>{{$usuario->dni_cif}}</td>
+
+          <td> @if ($usuario->isActive == 1)
+              <div class="custom-control custom-checkbox">
+                  <input type="checkbox" class="custom-control-input" id="isActive" name="isActive" value="isActive" checked>
+                  <label class="custom-control-label" for="isActive"></label>
+                </div>
+          @else
+          <div class="custom-control custom-checkbox">
+              <input type="checkbox" class="custom-control-input" id="isActive" name="isActive" value="isActive" >
+              <label class="custom-control-label" for="isActive"></label>
+
+
+
+
+              </div>
+
+          @endif
+      </td>
+
+
+
+
+
+      </tbody>
+       @endforeach
+      </table>
+
+
 </div>
 
-<table class="table">
-      <tr>
-        <th scope="col">ID</th>
-        <th scope="col">UserName</th>
-        <th scope="col">RealName</th>
-
-        <th scope="col">UserType</th>
-
-
-        <th>Dni/Cif</th>
-
-        <th>Active</th>
-
-
-      </tr>
-
-    <tbody>
-
-        @foreach ($usuarios as $usuario)
-       <td>{{$usuario->id_user}}</td>
-       <td>{{$usuario->user_name}}</td>
-       <td>{{$usuario->real_name}}</td>
-
-     <td>{{$usuario->user_type->type_name}}</td>
-
-
-        <td>{{$usuario->dni_cif}}</td>
-
-        <td> @if ($usuario->isActive == 1)
-            <div class="custom-control custom-checkbox">
-                <input type="checkbox" class="custom-control-input" id="isActive" name="isActive" value="isActive" checked>
-                <label class="custom-control-label" for="isActive"></label>
-              </div>
-        @else
-        <div class="custom-control custom-checkbox">
-            <input type="checkbox" class="custom-control-input" id="isActive" name="isActive" value="isActive" >
-            <label class="custom-control-label" for="isActive"></label>
+</div>
 
 
 
-
-            </div>
-
-        @endif
-    </td>
-
-
-        <td>{{$usuario->size}}</td>
-        <td>{{$usuario->location}}</td>
-        <td>{{$usuario->fed}}</td>
-        <td>
-            {{-- <form method="GET" action="{{action( [App\Http\Controllers\UsuarioController::class,'edit'],['usuario' => $usuario->id_user] )}}">
-                @csrf
-
-
-
-                <button class="btn btn-primary" type="submit">Editar</button>
-
-            </form> --}}
-
-        </td>
-        <td>
-            {{-- <form method="POST" action=" {{ action( [App\Http\Controllers\UsuarioController::class,'destroy'],['usuario' => $usuario->user_id] )}}">
-                @csrf
-
-                @method('DELETE')
-
-                <button class="btn btn-danger" type="submit">Borrar</button>
-                </form> --}}
-         </td>
-
-    </tbody>
-     @endforeach
-    </table>
 
 
 
